632215be9b7a2cb96a492a3d614160666c873f0d
Kristian Skov <kris701kj@gmail.com>
2026-02-16 12:52:41 +0100

    Added more tests

---
+++ Actions/DatabaseSharp/ActChain.Actions.DatabaseSharp.Tests/Actions/SerializationTests.cs
@@ -0,0 +1,46 @@
+﻿using ActChain.Actions.DatabaseSharp.Actions;
+using ActChain.Models.Actions;
+using ActChain.Models.Contexts;
+using ActChain.Tools.Extensions;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace ActChain.Actions.DatabaseSharp.Tests.Actions
+{
+	[TestClass]
+	public class SerializationTests
+	{
+		public static IEnumerable<object[]> InputModels()
+		{
+			yield return new object[] {
+				new FetchItemsFromDatabaseAction("abc", "a", "sp_a", new Dictionary<string, string>(), "FetchItemsFromDatabaseAction"),
+				"TestFiles/Actions/FetchItemsFromDatabaseAction_Serialization_Expected.json" };
+			yield return new object[] {
+				new InsertChainFromDatabaseAction("abc", "a", "sp_a", new Dictionary<string, string>()),
+				"TestFiles/Actions/InsertChainFromDatabaseAction_Serialization_Expected.json" };
+			yield return new object[] {
+				new InsertItemToDatabaseAction("abc", "a", "sp_a", new Dictionary<string, string>()),
+				"TestFiles/Actions/InsertItemToDatabaseAction_Serialization_Expected.json" };
+		}
+
+		[TestMethod]
+		[DynamicData(nameof(InputModels))]
+		public void Can_Serialize(IAIAction input, string expectedFile)
+		{
+			// ARRANGE
+			var options = new JsonSerializerOptions()
+			{
+				TypeInfoResolver = new DefaultJsonTypeInfoResolver().WithAddedModifier(JsonExtensions.AddNativePolymorphicTypInfo)
+			};
+
+			// ACT
+			var actualText = JsonSerializer.Serialize(input, options);
+
+			// ASSERT
+			var expectedFileText = File.ReadAllText(expectedFile);
+			var expectedObject = JsonSerializer.Deserialize<IAIAction>(expectedFileText, options);
+			var expectedText = JsonSerializer.Serialize(expectedObject, options);
+			Assert.AreEqual(expectedText, actualText);
+		}
+	}
+}
---
+++ Actions/DatabaseSharp/ActChain.Actions.DatabaseSharp.Tests/Executors/SerializationTests.cs
@@ -0,0 +1,45 @@
+﻿using ActChain.Actions.DatabaseSharp.Executors;
+using ActChain.Models.Executors;
+using ActChain.Tools.Extensions;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace ActChain.Actions.DatabaseSharp.Tests.Executors
+{
+	[TestClass]
+	public class SerializationTests
+	{
+		public static IEnumerable<object[]> InputModels()
+		{
+			yield return new object[] {
+				new FetchItemsFromDatabaseExecutor("a", ""),
+				"TestFiles/Executors/FetchItemsFromDatabaseExecutor_Serialization_Expected.json" };
+			yield return new object[] {
+				new InsertChainFromDatabaseExecutor("a", ""),
+				"TestFiles/Executors/InsertChainFromDatabaseExecutor_Serialization_Expected.json" };
+			yield return new object[] {
+				new InsertItemToDatabaseExecutor("a", ""),
+				"TestFiles/Executors/InsertItemToDatabaseExecutor_Serialization_Expected.json" };
+		}
+
+		[TestMethod]
+		[DynamicData(nameof(InputModels))]
+		public void Can_Serialize(IActionExecutor input, string expectedFile)
+		{
+			// ARRANGE
+			var options = new JsonSerializerOptions()
+			{
+				TypeInfoResolver = new DefaultJsonTypeInfoResolver().WithAddedModifier(JsonExtensions.AddNativePolymorphicTypInfo)
+			};
+
+			// ACT
+			var actualText = JsonSerializer.Serialize(input, options);
+
+			// ASSERT
+			var expectedFileText = File.ReadAllText(expectedFile);
+			var expectedObject = JsonSerializer.Deserialize<IActionExecutor>(expectedFileText, options);
+			var expectedText = JsonSerializer.Serialize(expectedObject, options);
+			Assert.AreEqual(expectedText, actualText);
+		}
+	}
+}
