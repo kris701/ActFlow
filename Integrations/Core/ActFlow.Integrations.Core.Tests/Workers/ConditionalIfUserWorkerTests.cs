0e2a25e2eee1b7249dbe24f6ab3850e29693d778
Kristian Skov <kris701kj@gmail.com>
2026-03-06 11:01:14 +0100

    Added tests for core Workers

---
+++ Integrations/Core/ActFlow.Integrations.Core.Tests/Workers/ConditionalIfUserWorkerTests.cs
@@ -0,0 +1,37 @@
+﻿using ActFlow.Integrations.Core.Activities;
+using ActFlow.Integrations.Core.Workers;
+using ActFlow.Models.Contexts;
+using ActFlow.Models.Workflows;
+
+namespace ActFlow.Integrations.Core.Tests.Workers
+{
+	[TestClass]
+	public class ConditionalIfUserWorkerTests
+	{
+		[TestMethod]
+		[DataRow("abc", "abc", ConditionalComparerTypes.Equal, "act2")]
+		[DataRow("abc", "abc", ConditionalComparerTypes.NotEqual, "act3")]
+		[DataRow("abc", "abc", ConditionalComparerTypes.Contains, "act2")]
+		[DataRow("abc", "123", ConditionalComparerTypes.NotEqual, "act2")]
+		[DataRow("abc", "123", ConditionalComparerTypes.Equal, "act3")]
+		public async Task Can_Execute(string user, string right, ConditionalComparerTypes compare, string expected)
+		{
+			// ARRANGE
+			var worker = new ConditionalUserWorker("", 10);
+			var activity = new ConditionalUserActivity("act", "", "", right, compare, "act2", "act3");
+
+			// ACT
+			Task.Run(async () =>
+			{
+				await Task.Delay(100);
+				activity.UserInput = user;
+			});
+
+			var result = await worker.Execute(activity, new WorkflowState(), new CancellationToken(), "");
+
+			// ASSERT
+			Assert.IsExactInstanceOfType<EmptyContext>(result.Context);
+			Assert.AreEqual(expected, result.TargetActivity);
+		}
+	}
+}
---
+++ Integrations/Core/ActFlow.Integrations.Core.Tests/Workers/ConditionalIfWorkerTests.cs
@@ -0,0 +1,31 @@
+﻿using ActFlow.Integrations.Core.Activities;
+using ActFlow.Integrations.Core.Workers;
+using ActFlow.Models.Contexts;
+using ActFlow.Models.Workflows;
+
+namespace ActFlow.Integrations.Core.Tests.Workers
+{
+	[TestClass]
+	public class ConditionalIfWorkerTests
+	{
+		[TestMethod]
+		[DataRow("abc", "abc", ConditionalComparerTypes.Equal, "act2")]
+		[DataRow("abc", "abc", ConditionalComparerTypes.NotEqual, "act3")]
+		[DataRow("abc", "abc", ConditionalComparerTypes.Contains, "act2")]
+		[DataRow("abc", "123", ConditionalComparerTypes.NotEqual, "act2")]
+		[DataRow("abc", "123", ConditionalComparerTypes.Equal, "act3")]
+		public async Task Can_Execute(string left, string right, ConditionalComparerTypes compare, string expected)
+		{
+			// ARRANGE
+			var worker = new ConditionalIfWorker("");
+			var activity = new ConditionalIfActivity("act", "", left, right, compare, "act2", "act3");
+
+			// ACT
+			var result = await worker.Execute(activity, new WorkflowState(), new CancellationToken(), "");
+
+			// ASSERT
+			Assert.IsExactInstanceOfType<EmptyContext>(result.Context);
+			Assert.AreEqual(expected, result.TargetActivity);
+		}
+	}
+}
